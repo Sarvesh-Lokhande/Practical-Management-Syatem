8fe59e1788800fa0b48b0f30a21e5aba4846b97c
Sarvesh-Lokhande <sarveshlokhande2004@gmail.com>
2024-11-23 00:28:12 +0530

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+import dbConnect from "../config/database.js";
+
+import router from "../routes/feedbackRoutes.js";
+
+const app = express();
+
+const PORT = 3000;
+app.use(express.json());
+app.use("/api/v1",router) 
+
+dbConnect();
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`);
+})
